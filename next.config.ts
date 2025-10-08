c4f203b4ec4771ac71cd1e2a25947142973dfc77
= <jiryanfarokhi@gmail.com>
2025-10-08 21:11:54 +0700

    update

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,5 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: 'export',
-  basePath: '/my_portfolio',  // sesuaikan dengan nama repo
-  images: {
-    unoptimized: true,        // supaya export jalan lancar di GitHub Pages
-  },
-};
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {};
 
 export default nextConfig;
